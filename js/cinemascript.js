4b12a3f11da0a8643759e99769c1f8671738de45
Josip Bošnjak <jbosnjak@unipu.hr>
2019-08-03 12:13:29 +0200

    added days
    added how much days does have one month in liquid year

--- js/cinemascript.js
+++ js/cinemascript.js
@@ -1,6 +1,26 @@
-window.onload=function getMonths(){
+window.onload=function getDaysInMonths(){
   var months = ["January", "February", "March","April","May","June","July","August","September","October","November","December"];
-  var reference=document.getElementById("schedule").innerHTML = months;
+  var daysInMonths=[31,28,31,30,31,30,31,31,30,31,30,31];
+  var monthsLength=months.length;
+  var days=[];
+  for(var i=0;i<monthsLength;i++){
+    for(var j=0;j<daysInMonths.length;j++){
+      if(i==j) {
+          days+=months[i]+"="+daysInMonths[j]+"<br>";
+          break;
+
+      }else {continue};
+    }
+
+  }
+  /*
+ for(var i=0;i<monthsLength;i++){
+   month+=months[i]+",";
+   if(i==3) {break;};
+
+ }
+ */
+  var reference=document.getElementById("schedule").innerHTML = days;
   return reference;
 
 
