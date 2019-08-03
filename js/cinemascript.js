458063ef3bb986181afc5bc92e6f981b71585469
Josip Bošnjak <jbosnjak@unipu.hr>
2019-08-03 14:46:23 +0200

    added posters
    added posters from the movies also added movie description at the site.

--- js/cinemascript.js
+++ js/cinemascript.js
@@ -1,28 +1,33 @@
 window.onload=function getDaysInMonths(){
-  var months = ["January", "February", "March","April","May","June","July","August","September","October","November","December"];
-  var daysInMonths=[31,28,31,30,31,30,31,31,30,31,30,31];
-  var monthsLength=months.length;
-  var days=[];
-  for(var i=0;i<monthsLength;i++){
-    for(var j=0;j<daysInMonths.length;j++){
-      if(i==j) {
-          days+=months[i]+"="+daysInMonths[j]+"<br>";
-          break;
+ var movies=[" Spider-man far from home "," The lion king "," Aladdin "," Angel has fallen "," The nightingale ",
+" Once upon a time in Hollywood "," Fast & Furious: Hobbs & Shaw "," Killerman "];
+var movieType=["Science fiction","Family","Science fiction","Action","Mistery","Comedy","Action","Action"];
+var images=["img/spidermanfaraway.jpg","img/lionking.jpg","img/alladin.jpg","img/angelhasfallen.jpg",
+"img/thenightingale.jpg","img/onceuponatimeinholly.jpg","img/hobbsshaw.jpg","img/killerman.jpg"];
+var alt=["spiderman","lionking","alladin","angelhasfallen","nightingale","hollywood","hobbsshaw","killerman"];
+var duration=[" 2h 9min","1h 58min","2h 8min", " 2h 8min"," 2h 8min"," 2h 16min"," 2h 41min"," 2h 15min"," 2h 0min"];
+var source=[];
+for(var i=0;i<images.length;i++){
+  source[i]="<img src='"+images[i]+"' alt='"+alt[i]+"'/>";
+}
+var table="<table>";
+table+="<tr>";
+table+="<td>";
+table+="<strong>Movie:</strong>";
+table+="</td>";
+table+="</tr>";
 
-      }else {continue};
-    }
+for(var k=0;k<movies.length;k++){
+  table+="<tr><td>"+source[k];
+  table+=movies[k];
+  table+=movieType[k];
+  table+=duration[k]+"</td>";
+  table+="</tr>";
 
-  }
-  var table=[];
-    /*
- for(var i=0;i<monthsLength;i++){
-   month+=months[i]+",";
-   if(i==3) {break;};
+}
 
- }
- */
-  var reference=document.getElementById("schedule").innerHTML = days;
-  return reference;
-
+table+="</table>";
+var reference=document.getElementById("schedule").innerHTML=table;
+return reference;
 
 }
