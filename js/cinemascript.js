01f001e262ee9c7fc78545ea39c50b18ef90f0bd
Josip Bošnjak <jbosnjak@unipu.hr>
2019-08-03 11:37:56 +0200

    added months
    added javascript function which returns months in one calendar year

---
+++ js/cinemascript.js
@@ -0,0 +1,7 @@
+window.onload=function getMonths(){
+  var months = ["January", "February", "March","April","May","June","July","August","September","October","November","December"];
+  var reference=document.getElementById("schedule").innerHTML = months;
+  return reference;
+
+
+}
