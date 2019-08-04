88bf3d4774fc57f2ac25cc0a5df7081219ccafda
Josip Bošnjak <jbosnjak@unipu.hr>
2019-08-04 21:38:22 +0200

    table done
    table which shows movies and date and time of projections in cinema is done. Next thing needs to be done is interaction between user and web site.

--- js/cinemascript.js
+++ js/cinemascript.js
@@ -11,23 +11,53 @@ for(var i=0;i<images.length;i++){
   source[i]="<img src='"+images[i]+"' alt='"+alt[i]+"'/>";
 }
 var table="<table>";
-table+="<tr>";
-table+="<td>";
-table+="<strong>Movie:</strong>";
-table+="</td>";
-table+="</tr>";
-
+table+="<tr><th>Movie:</th><th>Monday, 5.8.2019.</th><th>Tuesday, 6.8.2019.</th><th>Wednesday, 7.8.2019.</th><th>Thursday, 8.8.2019.</th><th>Friday, 9.8.2019.</th><th>Saturday, 10.8.2019.</th><th>Sunday, 11.8.2019.</th></tr>";
 for(var k=0;k<movies.length;k++){
   table+="<tr><td>"+source[k];
-  table+="<p class='info'>"+movies[k]+"</p>";
-  table+="<p class='info'>"+movieType[k]+"</p>";
-  table+="<p class='info'>"+duration[k]+"</p></td>";
-  table+="</tr>";
+  table+="<p id='fnt' class='info'><h2>"+movies[k]+"</h2></p>";
+  table+="<p id='fnt' class='info'>"+movieType[k]+"</p>";
+  table+="<p id='fnt' class='info'>"+duration[k]+"</p></td>";
+  table+="<td><p class='info'>"+"12:25"+"</p>";
+  table+="<p class='info'>"+"16:40"+"</p>";
+  table+="<p class='info'>"+"18:50"+"</p></td>";
 
+
+  table+="<td><p class='info'>"+"13:25"+"</p>";
+  table+="<p class='info'>"+"17:40"+"</p>";
+  table+="<p class='info'>"+"19:50"+"</p></td>";
+
+  table+="<td><p class='info'>"+"15:25"+"</p>";
+  table+="<p class='info'>"+"18:40"+"</p>";
+  table+="<p class='info'>"+"20:50"+"</p></td>";
+
+
+  table+="<td><p class='info'>"+"15:25"+"</p>";
+  table+="<p class='info'>"+"16:40"+"</p>";
+  table+="<p class='info'>"+"20:50"+"</p></td>";
+
+  table+="<td><p class='info'>"+"12:25"+"</p>";
+  table+="<p class='info'>"+"16:40"+"</p>";
+  table+="<p class='info'>"+"18:50"+"</p></td>";
+
+  table+="<td><p class='info'>"+"12:25"+"</p>";
+  table+="<p class='info'>"+"16:40"+"</p>";
+  table+="<p class='info'>"+"18:50"+"</p></td>";
+
+  table+="<td><p class='info'>"+"12:25"+"</p>";
+  table+="<p class='info'>"+"16:40"+"</p>";
+  table+="<p class='info'>"+"18:50"+"</p></td>";
+
+
+  table+="</tr>";
 }
+
+
 
 table+="</table>";
 var reference=document.getElementById("schedule").innerHTML=table;
 return reference;
 
+}
+function markGreen(){
+  return document.getElementById('markMovie').style.backgroundColor="green";
 }
