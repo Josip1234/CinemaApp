8249f6178a6fdc59ed646a54e01a6949f3fcef1e
Josip Bošnjak <jbosnjak@unipu.hr>
2019-08-12 17:40:17 +0200

    set attribute from json data

--- js/cinemascript.js
+++ js/cinemascript.js
@@ -7,10 +7,17 @@ window.onload=function makeTable(){
 
     var divs = document.getElementsByClassName('tr');
     var divs2 = document.getElementsByClassName('info1');
+    var divs3 = document.getElementsByClassName('info2');
+    var divs4 = document.getElementsByClassName('info3');
+    var divs5 = document.getElementsByClassName('imgs');
 
     for(var i = 0; i<divs.length;i++) {
         divs[i].setAttribute("id",data.movies[i].name);
         divs2[i].innerHTML=data.movies[i].name;
+        divs3[i].innerHTML=data.movie[i].type;
+        divs4[i].innerHTML=data.dur[i].duration;
+        divs5[i].setAttribute("src",data.images[i].img);
+        divs5[i].setAttribute("alt",data.alternative[i].alt);
     }
 
 
@@ -23,30 +30,21 @@ window.onload=function makeTable(){
 
 
 
- var movies=[" Spider-man far from home "," The lion king "," Aladdin "," Angel has fallen "," The nightingale ",
-" Once upon a time in Hollywood "," Fast & Furious: Hobbs & Shaw "," Killerman "];
 
-var movieType=["Science fiction","Family","Science fiction","Action","Mistery","Comedy","Action","Action"];
-var images=["img/spidermanfaraway.jpg","img/lionking.jpg","img/alladin.jpg","img/angelhasfallen.jpg",
-"img/thenightingale.jpg","img/onceuponatimeinholly.jpg","img/hobbsshaw.jpg","img/killerman.jpg"];
-var alt=["spiderman","lionking","alladin","angelhasfallen","nightingale","hollywood","hobbsshaw","killerman"];
-var duration=[" 2h 9min"," 1h 58min"," 2h 8min", " 2h 8min"," 2h 8min"," 2h 16min"," 2h 41min"," 2h 15min"," 2h 0min"];
-var source=[];
+
 var days=["Monday,5.8.2019","Tuesday, 6.8.2019.","Wednesday, 7.8.2019.","Thursday,8.8.2019.","Friday,9.8.2019.","Saturday,10.8.2019.","Sunday, 11.8.2019."];
 var cinema=["A","B","C","D", "E","F","G","H"];
 var time=["12:25","16:40","18:50","13:25","17:40","19:50","15:25","18:40","20:50","15:25","16:40","20:50","12:25","16:40","18:50","12:25","16:40","18:50","12:25","16:40","18:50"];
-for(var i=0;i<images.length;i++){
-  source[i]="<img src='"+images[i]+"' alt='"+alt[i]+"'/>";
-}
+
 var table="<table class='table-hover'>";
 table+="<tr><th>Movie:</th><th id='"+days[0]+"' onClick='chosenDays(this.id)'>"+days[0]+"</th><th id='"+days[1]+"' onClick='chosenDays(this.id)'>"+days[1]+"</th><th id='"+days[2]+"' onClick='chosenDays(this.id)'>"+days[2]+"</th><th id='"+days[3]+"' onClick='chosenDays(this.id)'>"+days[3]+"</th><th id='"+days[4]+"' onClick='chosenDays(this.id)'>"+days[4]+"</th><th id='"+days[5]+"' onClick='chosenDays(this.id)'>"+days[5]+"</th><th id='"+days[6]+"' onClick='chosenDays(this.id)'>"+days[6]+"</th></tr>";
 
 
 for(var k=0;k<8;k++){
-  table+="<tr class='tr' id='' onClick='showdata(this.id)'><td>"+source[k];
+  table+="<tr class='tr' id='' onClick='showdata(this.id)'><td>"+"<img class='imgs' src='' alt='' />";
   table+="<p id='fnt'><h2 class='info1'>"+''+"</h2></p>";
-  table+="<p id='fnt' class='info'>"+movieType[k]+"</p>";
-  table+="<p id='fnt' class='info'>"+duration[k]+"</p>";
+  table+="<p id='fnt' class='info2'>"+''+"</p>";
+  table+="<p id='fnt' class='info3'>"+''+"</p>";
   table+="<p id='fnt' class='info'>Cinema:"+cinema[k]+"</p></td>";
   table+="<td><p id='"+time[0]+"' class='info' onClick='chosenTime(this.id)'>"+time[0]+"</p>";
   table+="<p id='"+time[1]+"' class='info' onClick='chosenTime(this.id)'>"+time[1]+"</p>";
